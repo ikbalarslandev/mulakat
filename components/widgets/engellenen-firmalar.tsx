e8bd59af17b5df86d0ed36b3276eb2f6aa33a1dc
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-03-20 13:01:04 +0300

    Engellenen Firmalar

--- components/widgets/engellenen-firmalar.tsx
+++ components/widgets/engellenen-firmalar.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { FaBan } from "react-icons/fa";
+import { PiWarningCircleBold } from "react-icons/pi";
+
 const EngellenenFirmalar = () => {
   return (
-    <div>
-      <h1>Engellenen Firmalar</h1>
+    <div className="mt-5 flex flex-col bg-gray-100">
+      <Button className=" mt-5 mr-2 mx-2 ml-auto  flex gap-2 p-2 ">
+        <FaBan className=" text-white w-5 h-5" />
+        <p> Firma Engelle</p>
+      </Button>
+
+      <div className="bg-white shadow-md  mt-5  flex items-center gap-5 mx-5 rounded-md">
+        <PiWarningCircleBold
+          className="w-10 h-10 bg-red-600 text-white  py-2 px-2 
+rounded-l-lg"
+        />
+        <p>Engellenen firma bulunamadı</p>
+      </div>
     </div>
   );
 };
