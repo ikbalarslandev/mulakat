688440ee0e48d8a59da24629549031bddbc961eb
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-03-20 11:39:47 +0300

    Sms bildirimleri

--- components/widgets/sms-bildirimleri.tsx
+++ components/widgets/sms-bildirimleri.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
+
+const IstiyorumSwitch = () => {
+  const [iwant, setIwant] = useState(false);
+  const handleIwant = () => {
+    setIwant(!iwant);
+  };
+  return (
+    <div className="flex items-center space-x-2">
+      <Label className="w-20" htmlFor="airplane-mode">
+        {iwant ? "istiyorum" : "istemiyorum"}
+      </Label>
+      <Switch onClick={handleIwant} checked={iwant} id="airplane-mode" />
+    </div>
+  );
+};
+
+const SectionTemplate = ({ desc }: any) => {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <p className="text-gray-500 text-sm">{desc}</p>
+      <IstiyorumSwitch />
+    </div>
+  );
+};
+
 const SmsBildirimleri = () => {
   return (
-    <div>
-      <h1>Sms Bildirimleri</h1>
+    <div className="flex flex-col gap-5 items-center m-5">
+      <SectionTemplate desc="SMS bildirimi almak istiyor musun?" />
+
+      <Button variant="kaydet" className="ml-auto mt-5">
+        Kaydet
+      </Button>
     </div>
   );
 };
